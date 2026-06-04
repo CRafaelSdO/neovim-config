10ce091e765ba48924c320f1fc391321db92ed10
Rafael Oliveira <crafaelsdo@gmail.com>
2026-06-04 00:52:56 -0300

    feat: LSPs
    Adiciona e configura os plugins 'mason-lspconfig.nvim', 'nvim-lspconfig'
    e 'SchemaStore.nvim' para gerenciamento e configuração de LSPs.

--- lua/crso/meta/languages.lua
+++ lua/crso/meta/languages.lua
@@ -4,14 +4,81 @@ M._languages_meta = {
 	vim = {},
 	vimdoc = {},
 	query = {},
-	markdown = {},
-	markdown_inline = {},
-	lua = {},
-	yaml = {},
+
+	markdown = {
+		lsp = "marksman",
+	},
+
+	markdown_inline = {
+		lsp = "marksman",
+	},
+
+	lua = {
+		lsp = "lua_ls",
+	},
+
+	yaml = {
+		lsp = "yamlls",
+	},
+}
+
+M._lsps_opts = {
+	lua_ls = {
+		settings = {
+			Lua = {
+				diagnostics = { globals = { "vim" } },
+
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+					checkThirdParty = false,
+				},
+
+				telemetry = { enable = false },
+			},
+		},
+	},
+
+	yamlls = {
+		settings = {
+			yaml = {
+				validate = true,
+				format = { enabled = false },
+				customTags = { "!reference sequence" },
+
+				schemas = function()
+					return require("schemastore").yaml.schemas()
+				end,
+			},
+		},
+	},
 }
 
 function M:languages()
 	return vim.tbl_keys(self._languages_meta)
+end
+
+function M:lsps()
+	local lsps = {}
+
+	for _, meta in pairs(self._languages_meta) do
+		if meta.lsp then
+			table.insert(lsps, meta.lsp)
+		end
+	end
+
+    return lsps
+end
+
+function M:lsps_opts()
+	local lsps_opts = {}
+
+	for _, meta in pairs(self._languages_meta) do
+		if meta.lsp then
+			lsps_opts[meta.lsp] = self._lsps_opts[meta.lsp] and self._lsps_opts[meta.lsp] or {}
+		end
+	end
+
+	return lsps_opts
 end
 
 return M
---
+++ lua/crso/plugins/editor/lsp.lua
@@ -0,0 +1,69 @@
+local set = vim.keymap.set
+
+local function on_init(client, _)
+	if client:supports_method("textDocument/semanticTokens") then
+		client.server_capabilities.semanticTokensProvider = nil
+	end
+end
+
+local function on_attach(_, bufnr)
+	local function opts(desc)
+		return { buffer = bufnr, desc = "LSP " .. desc }
+	end
+
+	---------------------------------------------------------------------------
+	-- NAVIGATION AND DOCUMENTATION
+	---------------------------------------------------------------------------
+	set("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
+	set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
+	set("n", "K", vim.lsp.buf.hover, opts("Hover Documentation"))
+	set("n", "gT", vim.lsp.buf.type_definition, opts("Go to type definition"))
+
+	---------------------------------------------------------------------------
+	-- CODE ACTIONS
+	---------------------------------------------------------------------------
+	set("n", "<leader>cr", vim.lsp.buf.rename, opts("Rename Symbol"))
+	set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code Action"))
+
+	---------------------------------------------------------------------------
+	-- WORKSPACE
+	---------------------------------------------------------------------------
+	set("n", "<leader>cwa", vim.lsp.buf.add_workspace_folder, opts("Workspace: Add folder"))
+	set("n", "<leader>cwr", vim.lsp.buf.remove_workspace_folder, opts("Workspace: Remove folder"))
+
+	set("n", "<leader>cwl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, opts("Workspace: List folders"))
+end
+
+return {
+	{
+		"mason-org/mason-lspconfig.nvim",
+
+		event = { "BufReadPre", "BufNewFile" },
+
+		dependencies = {
+			"mason-org/mason.nvim",
+			"neovim/nvim-lspconfig",
+			"b0o/SchemaStore.nvim",
+		},
+
+		opts = {
+			ensure_installed = require("crso.meta.languages"):lsps(),
+			automatic_enable = true,
+		},
+
+		config = function(_, opts)
+			vim.lsp.config("*", {
+				on_init = on_init,
+				on_attach = on_attach,
+			})
+
+			for lsp, lsp_opts in pairs(require("crso.meta.languages"):lsps_opts()) do
+				vim.lsp.config(lsp, lsp_opts)
+			end
+
+			require("mason-lspconfig").setup(opts)
+		end,
+	},
+}
