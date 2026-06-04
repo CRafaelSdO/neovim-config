9f8ef28f6e0ee16984697dafcef9227482e109da
Rafael Oliveira <crafaelsdo@gmail.com>
2026-06-04 03:11:31 -0300

    feat: Indent lines
    Adiciona e configura o plugin 'indent-blankline.nvim' para aplicar
    linhas de identação, facilitando a leitura.

---
+++ lua/crso/plugins/ui/indent-blankline.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"lukas-reineke/indent-blankline.nvim",
+
+		main = "ibl",
+		event = { "BufReadPost", "BufNewFile" },
+
+		opts = {
+			indent = { char = "│" },
+
+			exclude = {
+				filetypes = {
+					"help",
+					"lazy",
+					"mason",
+					"notify",
+				},
+			},
+
+			scope = {
+				enabled = true,
+				show_start = false,
+			},
+		},
+	},
+}
