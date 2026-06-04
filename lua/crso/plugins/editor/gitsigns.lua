9d0b572b78093f7fa5ac06252d0038eb2dcb85a4
Rafael Oliveira <crafaelsdo@gmail.com>
2026-06-04 01:20:48 -0300

    feat: gitsigns.nvim
    Adiciona e configura o plugin 'gitsigns.nvim' para obter retorno visual
    referênte a alteração nos arquivos em repositórios do git.

---
+++ lua/crso/plugins/editor/gitsigns.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "lewis6991/gitsigns.nvim",
+
+        event = { "BufReadPost", "BufNewFile" },
+
+        opts = {
+            preview_config = { border = "rounded" },
+        },
+    },
+}
